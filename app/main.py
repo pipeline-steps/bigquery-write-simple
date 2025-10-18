8bd8fcc8ee95e76e1a1c23bf63895ff2624d25a4
Jan Kneissler <jan.kneissler@breuninger.de>
2025-10-18 20:09:30 +0200

    use pandas_gbq

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import timeit
-from google.cloud.bigquery.client import pandas
+import pandas
+import pandas_gbq
 from steputil import StepArgs, StepArgsBuilder
 
 
@@ -11,7 +12,7 @@ def main(step: StepArgs):
     # Writing to BQ table
     start_time = timeit.default_timer()
     print(f"Loading data to {step.config.tableId} (using project {step.config.billingProject} for billing)")
-    pandas.to_gbq(df, step.config.tableId, project_id=step.config.tableId, if_exists=step.config.tableId.ifExists)
+    pandas_gbq.to_gbq(df, step.config.tableId, project_id=step.config.tableId, if_exists=step.config.tableId.ifExists)
     execution_time = timeit.default_timer() - start_time
     print(f"Uploaded {len(df.columns)} columns and {len(df)} rows in in {execution_time:.1f} seconds.")
 
